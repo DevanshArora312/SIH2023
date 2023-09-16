831dd379f0fefb9f66a2b8ade8e412c544bd463f
DevanshArora312 <devansharora312@gmail.com>
2023-09-16 18:03:23 +0530

    added login and signup from chatapp

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import {Routes,Route,BrowserRouter } from "react-router-dom";
+import Login from "./pages/login"
+import SignUp from "./pages/signup"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element={<><div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,7 +31,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+</>}/>
+        <Route path='login' element ={<Login/>}/>
+        <Route path='signup' element ={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
